c5dc202d26cee9c4cc6bc6dfd6b2591fff6b8624
Priya <pvaidya56@gmail.com>
2020-07-10 10:44:19 -0700

    completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,16 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    #empty directory for nums list, empty list for negative numbers
+    nums_list = dict()
+    negative_nums = []
 
-    return result
+    #check if numbers are not zero AND negative
+    for num in a:
+        nums_list[num] = 1
+        if num != 0 and -num in nums_list:
+            #append the number, absolute value
+            negative_nums.append(abs(num))
+
+    return negative_nums
 
 
 if __name__ == "__main__":
